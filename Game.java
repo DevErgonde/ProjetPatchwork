09558d6bd04d52ff447f035cdd9c0b81062a8c03
DevErgonde <msloss01@hotmail.com>
2022-11-08 23:31:44 +0100

    Continuation de la phase 1

--- Game.java
+++ Game.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Game{
   
   private final Player p1;
   private final Player p2;
+  private final ArrayList<Template> templates;
   
   public Game(String name1, String name2) {
+    Objects.requireNonNull(name1);
+    Objects.requireNonNull(name2);
     p1 = new Player(name1);
     p2 = new Player(name2);
+    templates = new ArrayList<Template>();
   }
   
 }
--- Player.java
+++ Player.java
@@ -3,10 +3,12 @@ import java.util.Objects;
 public class Player{
 	
   private final String name;
+  private final int buttons;
   
   public Player(String name) {
     Objects.requireNonNull(name);
     this.name = name;
+    this.buttons = 0;
   }
 }
 
